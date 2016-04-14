f1d430a4e30c878268517fa12b7280707545d301
Mike Buckley <mikebuckley.canada@gmail.com>
2016-04-13 20:52:27 -0700

    Added a bit of documentation

--- routes/memes.js
+++ routes/memes.js
@@ -6,7 +6,13 @@ var tmp = require('tmp');
 var shorturl = require('shorturl');
 var webshot = require('webshot');
 
-/* Save base64 to image and return path */
+/**
+ * Take in image, header and footer text and re-create the meme image, save and return a shortUrl.
+ * TODO: Compose this into utilities.
+ * @param imageUrl
+ * @param headerText
+ * @param footerText
+ */
 router.post('/', function (req, res) {
 	if (!req.body || !req.body.imageUrl || !req.body.headerText || !req.body.footerText) return res.sendStatus(400);
 
@@ -41,6 +47,9 @@ router.post('/', function (req, res) {
 
 });
 
+/**
+ * This is the route that renders the image used by the post above to take a screenshot...
+ */
 router.get('/', function(req, res, next) {
 
   res.render('meme', {
