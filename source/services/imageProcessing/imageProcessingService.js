121cd08e81ca72b598ed25d86078a03ac0212fee
Mike Buckley <mikebuckley.canada@gmail.com>
2016-05-02 20:38:23 -0700

    Let tmp create the folders if needed

--- source/services/imageProcessing/imageProcessingService.js
+++ source/services/imageProcessing/imageProcessingService.js
@@ -1,34 +1,34 @@
 var tmp = require('tmp');
-var iub = require("./../../utils/UriBuilder");
-var cr = require("./../../utils/CategoryResolver");
-var q = require("q");
+var iub = require('./../../utils/UriBuilder');
+var cr = require('./../../utils/CategoryResolver');
+var q = require('q');
 
 function imageProcessingSetup(req) {
-var deferrer = q.defer();
-    cr.createIfDirectoryDoesNotExist(req.body.category ? req.body.category : req.app.locals.DefaultPublicFolder).then(function (resolve) {
-        
-        if(resolve){
-            var result = {
-                memeCardUrl: iub.ImageUriBuilder.buildMemeUri(req),
+    var deferrer = q.defer();
+    // cr.createIfDirectoryDoesNotExist(req.body.category ? req.body.category : req.app.locals.DefaultPublicFolder).then(function (resolve) {
 
-                tmpName: tmp.tmpNameSync({
-                    template: '/memes/meme-XXXXXXXXX',
-                    keep: true
-                }),
-                outputFileName: './public/images/category/' + category + '/' + req.body.category + tmpName + '.png',
-                screenShotOpts: {
-                    shotSize: {
-                        width: 500,
-                        height: 500
-                    }
-                }
-            };
-            deferrer.resolve(result);
+    var category = (req.body.category) ? req.body.category : req.app.locals.DefaultPublicFolder;
+    console.log('USING CATEGORY', category);
+    var result = {
+        memeCardUrl: iub.ImageUriBuilder.buildMemeUri(req),
+
+        tmpName: tmp.tmpNameSync({
+            template: category + '/memes/meme-XXXXXXXXX',
+            keep: true
+        }),
+        outputFileName: './public/images/category/' + tmpName + '.png',
+        screenShotOpts: {
+            shotSize: {
+                width: 500,
+                height: 500
+            }
         }
-        
-    }, function(reject){
-        throw Error(reject);
-    });
+    };
+    deferrer.resolve(result);
+
+    // }, function(reject){
+    //     throw Error(reject);
+    // });
 };
 
 module.exports = {
