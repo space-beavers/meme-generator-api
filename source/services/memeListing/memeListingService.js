0a66fbe30823937895cd68584069a1a54da9f4d8
Mike Buckley <mikebuckley.canada@gmail.com>
2016-05-04 19:10:55 -0700

    Move memesListing service to async

--- source/services/memeListing/memeListingService.js
+++ source/services/memeListing/memeListingService.js
@@ -7,14 +7,13 @@ var iub = require('../../utils/UriBuilder');
 
 /**
  * Reads a category folder and returns the listings as an array of objects...
- * NOTE: Synchronous
+ * FIXME: replace fs.readdirSync with fs.readdir and use promise...
  * @param category Folder name where memes are stored.
  */
 function memeListingFromFolder(req, category) {
     var memesFolderPath = './public/uploads/memes/category/' + category;
 
     var memesResult = [];
-
     var files = fs.readdirSync(memesFolderPath);
     for (var i in files) {
         memesResult.push({
